9691d94d6b997314244ef2f8ced9557ff2a0edc0
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-03-27 21:25:10 +0100

    Add project files.

---
+++ Conversion from 2 to base 10/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Conversion_from_10_to_base_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please Enter The Value You Want To Convert:");
+            int Num2Covt = int.Parse(Console.ReadLine());
+            int ans = Num2Covt;
+            string result = "";
+            char[] charArray = result.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedResult = new string(charArray);
+
+           
+            while (ans >= 1)
+            {
+                int x = ans % 2;
+                ans = ans / 2;
+                result = result + x;
+            }
+            Console.WriteLine(reversedResult);
+        }
+    }
+}
